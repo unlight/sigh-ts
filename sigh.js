af8c2894bb4c15ef5def29f247eab8786dcc0d0a
Roman <s91630277@gmail.com>
2016-05-18 15:44:50 +0300

    Allow to pass all options of typescript

--- sigh.js
+++ sigh.js
@@ -2,12 +2,8 @@ var glob, babel, write;
 
 module.exports = function(pipelines) {
 	pipelines["source"] = [
-		glob({
-			basePath: "src"
-		}, "*.js"),
-		babel({
-			modules: "common"
-		}),
+		glob({basePath: "src"}, "*.js"),
+		babel({modules: "common"}),
 		write("lib")
 	];
 }
